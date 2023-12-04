0b3232e1ec8e85e6cbc715405d4539ab7104dc42
Josue Perez <josuepe03@hotmail.com>
2023-12-04 15:11:50 -0600

    Se agrego todo

--- src/screens/Services.jsx
+++ src/screens/Services.jsx
@@ -7,7 +7,7 @@ export default function Services() {
         <div className="w-full h-full ">
           <NavBarServices />
 
-          <section className="h-[90%] lg:mt-[8rem] xl:pt-10">
+          <section className="h-[90%]  xl:pt-10">
             <div className="flex w-full h-full justify-center items-center ">
               <div className="grid xl:grid-cols-3 lg:grid-cols-2 gap-6 h-full">
                 <div className="flex items-center">
@@ -28,33 +28,6 @@ export default function Services() {
                         <a
                           className="bg-[#8A0026] text-white font-extrabold text-2xl px-4 py-2 rounded-xl mt-2"
                           href="servicios2.html"
-                        >
-                          Leer Mas...
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flex items-center">
-                  <div className="lg:h-full xl:h-[70%] bg-[#242924] rounded-[3rem]">
-                    <div className="w-full h-full">
-                      <h1 className="h-[10%] uppercase text-[2.5rem] font-bold text-center my-[1.7rem] text-[#8A0026]">
-                        Ciberseguridad <br />
-                      </h1>
-                      <p className="h-[60%] pt-6 px-8 font-extrabold text-2xl text-white">
-                        * Creacion de <br />
-                        infraestructura TI <br />
-                        * Proteccion de datos <br />
-                        * Seguridad de sistemas y <br />
-                        redes <br />
-                        * Seguridad en redes <br />
-                        sociales.
-                      </p>
-                      <div className="flex justify-center">
-                        <a
-                          className="bg-[#8A0026] text-white font-extrabold text-2xl px-4 py-2 rounded-xl mt-2"
-                          href="servicios7.html"
                         >
                           Leer Mas...
                         </a>
@@ -88,7 +61,37 @@ export default function Services() {
                     </div>
                   </div>
                 </div>
+                
+                <div className="flex items-center">
+                  <div className="lg:h-full xl:h-[70%] bg-[#242924] rounded-[3rem]">
+                    <div className="w-full h-full">
+                      <h1 className="h-[10%] uppercase text-[2.5rem] font-bold text-center my-[1.7rem] text-[#8A0026]">
+                        Ciberseguridad <br />
+                      </h1>
+                      <p className="h-[60%] pt-6 px-8 font-extrabold text-2xl text-white">
+                        * Creacion de <br />
+                        infraestructura TI <br />
+                        * Proteccion de datos <br />
+                        * Seguridad de sistemas y <br />
+                        redes <br />
+                        * Seguridad en redes <br />
+                        sociales.
+                      </p>
+                      <div className="flex justify-center">
+                        <a
+                          className="bg-[#8A0026] text-white font-extrabold text-2xl px-4 py-2 rounded-xl mt-2"
+                          href="servicios7.html"
+                        >
+                          Leer Mas...
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
+
+
+              
               {/* <div className="text-[#8A0026] pl-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
